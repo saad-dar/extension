ceebad89c83d6cb8ecdfb81ec2dfb864fbacf17a
saadullah dar <saad.dar@funavry.com>
2023-04-10 17:59:51 -0400

    going to make seperate next component for javascript vulnerable components

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,5 @@
 var whoiser = require('whoiser')
 import axios from 'axios'
-import retire from 'retire';
-import fetch from 'node-fetch';
-import jsdom from 'jsdom';
-import request from 'request';
-const { JSDOM } = jsdom;
-
 export default async function handler(req, res) {
   // get IP/Domain reputation
   const options = {
@@ -23,57 +17,8 @@ export default async function handler(req, res) {
     // const autority = await axios.request(options);
     const domainInfo = await whoiser(req.query.keyword, {host, follow: 1});
     const response = {...domainInfo[host]}; //, ...autority.data.body
-
-    // extract javaScript vulnarable components
-    const websiteHtml = await scanWebsite(req.query.keyword);
-    const dom = new JSDOM(websiteHtml);
-    const scripts = dom.window.document.querySelectorAll('script');
-
-    const javaScriptComp = javaScriptComponents(scripts);
-    const vulnarableComp = vulnarableComponents(javaScriptComp);
-    console.log(vulnarableComp);
     res.status(200).json(response);
   } catch (error) {
     console.error(error);
-  }
-}
-
-async function scanWebsite(url) {
-  const response = await fetch(url);
-  const body = await response.text();
-  // console.log(body);
-  // const result = retire.scanUri(body);
-  return body;
-}
-
-async function javaScriptComponents(scripts) {
-  const javascript = [];
-  for (let i = 0; i < scripts.length; i++) {
-    const script = scripts[i];
-    if (script.src) {
-      // Fetch external script source code using request module
-      request(script.src, (error, response, body) => {
-        if (error) {
-          console.error(`request error: ${error}`);
-          return;
-        }
-        javascript.push(body);
-      });
-    } else {
-      // Add inline script source code to array
-      javascript.push(script.innerHTML);
-    }
-  }
-  return javascript;
-}
-
-async function vulnarableComponents(javascript) {
-  const vulnerableComponents = [];
-  let VulnerabilityResult = false;
-  for (let i = 0; i < javascript.length; i++ ) {
-    VulnerabilityResult = retire.isVulnerable(javascript[i]);
-    if(VulnerabilityResult) {
-      vulnerableComponents.push(javascript[i]);
-    }
   }
 }
